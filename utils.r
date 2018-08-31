c9dcb042bcc78e2f5d244cb6fbd8f9ae36494bcd
Daniel Bion Barreiros <bion@manifestohut.com>
2018-08-31 00:54:59 -0300

    fix perceptron

--- utils.r
+++ utils.r
@@ -100,10 +100,8 @@ predictPerceptron = function(models, test, sizeOfPool){
     for(i in 1:length(models[[1]])){
         pred[[i]] = matrix(0, sizeOfPool, nrow(test))
         for(j in 1:sizeOfPool){
-            # pred[[i]][j, ] = perceptron.test(test, models[[j]][[i]])
-            t = test[,models[[j]][[i]]$names]
-            p = predict(models[[j]][[i]], newdata = t)
-            pred[[i]][j, ] = ifelse(p[,2] < 0.50, 'false', 'true')
+            t = test[,models[[j]][[i]][[1]]]
+            pred[[i]][j, ] = perceptron.test(t, models[[j]][[i]][[2]])
         }
     }
 
@@ -145,47 +143,50 @@ perceptron.test = function(x, weights) {
 }
 
 perceptron.train = function(x, eta, niter) {
-        target = x[,ncol(x)]
-        x = x[, -ncol(x)]
+    target = x[,ncol(x)]
+    x = x[, -ncol(x)]
+    
 
-        y = rep (1, length(target))
-        y[target == "true"] = -1
-        
-        # initialize weight vector
-        weight <- rep(0, dim(x)[2] + 1)
-        errors <- rep(0, niter)
-        
-        
-        # loop over number of epochs niter
-        for (jj in 1:niter) {
-                
-                # loop through training data set
-                for (ii in 1:length(y)) {
-                        
-                        # Predict binary label using Heaviside activation 
-                        # function
-                        z <- sum(weight[2:length(weight)] * 
-                                         as.numeric(x[ii, ])) + weight[1]
-                        if(z < 0) {
-                                ypred <- -1
-                        } else {
-                                ypred <- 1
-                        }
-                        
-                        # Change weight - the formula doesn't do anything 
-                        # if the predicted value is correct
-                        weightdiff <- eta * (y[ii] - ypred) * 
-                                c(1, as.numeric(x[ii, ]))
-                        weight <- weight + weightdiff
-                        
-                        # Update error function
-                        if ((y[ii] - ypred) != 0.0) {
-                                errors[jj] <- errors[jj] + 1
-                        }
-                        
-                }
-        }
-        
-        # weight to decide between the two species 
-        return(weight)
+    y = rep (1, length(target))
+    y[target == "true"] = -1
+    
+    
+    # initialize weight vector
+    weight <- rep(0, dim(x)[2] + 1)
+    names = names(x)
+    errors <- rep(0, niter)
+    
+    
+    # loop over number of epochs niter
+    for (jj in 1:niter) {
+            
+            # loop through training data set
+            for (ii in 1:length(y)) {
+                    
+                    # Predict binary label using Heaviside activation 
+                    # function
+                    z <- sum(weight[2:length(weight)] * 
+                                        as.numeric(x[ii, ])) + weight[1]
+                    if(z < 0) {
+                            ypred <- -1
+                    } else {
+                            ypred <- 1
+                    }
+                    
+                    # Change weight - the formula doesn't do anything 
+                    # if the predicted value is correct
+                    weightdiff <- eta * (y[ii] - ypred) * 
+                            c(1, as.numeric(x[ii, ]))
+                    weight <- weight + weightdiff
+                    
+                    # Update error function
+                    if ((y[ii] - ypred) != 0.0) {
+                            errors[jj] <- errors[jj] + 1
+                    }
+                    
+            }
+    }
+    
+    # weight to decide between the two species 
+    return( list(names, weight))
 }
