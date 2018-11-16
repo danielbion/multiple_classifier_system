7425c7dcbe57a67e457ffff45b17b5031ce039e6
Daniel Bion Barreiros <bion@manifestohut.com>
2018-11-16 14:30:45 -0300

    New datasets

--- projeto/main.r
+++ projeto/main.r
@@ -1,8 +1,5 @@
-library('rpart')
 library('ROSE')
 library('caret')
-library('dbscan')
-library('naivebayes')
 library('randomForest')
 library('RWeka')
 
@@ -12,39 +9,49 @@ source('utils.r')
 
 NB = make_Weka_classifier("weka/classifiers/bayes/NaiveBayes")
 
-data = read.table('data3.csv', sep=',', header=TRUE)
-numOfFolds = 10
+experiments = function(data, classifier, rule){
+    numOfFolds = 10
+    
+    x = as.data.frame(apply(data[, -ncol(data)], 2, standardize))
+    x$target = data$target
+    
+    x = splitInFolds(x, numOfFolds)
+    auc = list()
 
-x = splitInFolds(data, numOfFolds)
+    for (i in 1:numOfFolds) {
+        trainSet = x[[i]]$train
+        testSet = x[[i]]$test
 
-for (i in 1:numOfFolds) {
-    trainSet = x[[i]]$train
-    testSet = x[[i]]$test
+        majorityClass = trainSet[which(trainSet$target == 'false'), ]
+        minorityClass = trainSet[which(trainSet$target == 'true'), ]
 
-    majorityClass = trainSet[which(trainSet$target == 'false'), ]
-    minorityClass = trainSet[which(trainSet$target == 'true'), ]
+        numOfBins = floor(nrow(majorityClass) / nrow(minorityClass))
 
-    numOfBins = floor(nrow(majorityClass) / nrow(minorityClass))
+        binsIdx = createFolds(majorityClass$target, k = numOfBins)
 
-    binsIdx = createFolds(majorityClass$target, k = numOfBins)
+        bins = list()
+        for(j in 1:length(binsIdx)){
+            bins[[j]] = rbind(majorityClass[binsIdx[[j]], ], minorityClass)
+            bins[[j]] = bootstrap(bins[[j]])
+        }
 
-    bins = list()
-    for(j in 1:length(binsIdx)){
-        bins[[j]] = rbind(majorityClass[binsIdx[[j]], ], minorityClass)
-        bins[[j]] = bootstrap(bins[[j]])
+        pool = list()
+        for(j in 1:length(bins)){
+            pool[[j]] = classifier(target ~ ., bins[[j]]) #IBK
+        }
+
+        p = predictPool(pool, testSet, rule)    
+        auc[[i]] = roc.curve(testSet$target, p, plotit = F)$auc
     }
 
-    for(j in 1:length(bins)){
-        #model = naive_bayes(bins[[j]], bins[[j]]$target)
-        #model = NB(target ~ ., bins[[j]]) # Naive Bayes
-        #model = J48(target ~ ., bins[[j]]) # C4.5
-        #model = JRip(target ~ ., bins[[j]]) # Ripper
-        #model = SMO(target ~ ., bins[[j]]) # SMO
-        #model = IBk(target ~ ., bins[[j]]) #IBK
-        model = randomForest(target ~ ., bins[[j]]) #IBK
-        model = randomForest(bins[[j]], bins[[j]]$target) #IBK
-
-        p = predict(model, testSet)
-        print(table(p, testSet$target))
-    }
+    return (auc)
 }
+
+data = read.table('data3.csv', sep=',', header=TRUE)
+
+naiveBayes_MajorityVote = experiments(data, NB, majorityVote)
+J48_MajorityVote = experiments(data, J48, majorityVote)
+JRip_MajorityVote = experiments(data, JRip, majorityVote)
+SMO_MajorityVote = experiments(data, SMO, majorityVote)
+IBK_MajorityVote = experiments(data, IBk, majorityVote)
+randomForest_MajorityVote = experiments(data, randomForest, majorityVote)
--- projeto/utils.r
+++ projeto/utils.r
@@ -16,331 +16,36 @@ splitInFolds = function(x, numOfFolds){
     return (splited)
 }
 
-testPair = function(classifier1, classifier2, x){
-    target = x[,ncol(x)]
-    x = x[, -ncol(x)]
-    n11 = 0
-    n00 = 0
-    n10 = 0
-    n01 = 0
-
-    for(i in 1:nrow(x)){
-        x1 = x[i,classifier1[[1]]]
-        pred1 = perceptron.test(x1, classifier1[[2]])
-
-        x2 = x[i,classifier2[[1]]]
-        pred2 = perceptron.test(x2, classifier2[[2]])
-
-        if(pred1 == target[i] && pred2 == target[i]){
-            n11 = n11 + 1
-        }else if(pred1 == target[i] && pred2 != target[i]){
-            n10 = n10 + 1
-        }else if(pred1 != target[i] && pred2 == target[i]){
-            n01 = n01 + 1
-        }else{
-            n00 = n00 + 1
-        }
-    }
-    return (list(n11, n10, n01, n00))
+standardize = function(x){
+    return((x - min(x)) / (max(x) - min(x)))
 }
 
-qStatistic = function(n11, n10, n01, n00, l){
-    if(n11 == l) {
-        return (0)
-    }
-    q = ((n11 * n00) - (n01 * n10)) / ((n11 * n00) + (n01 * n10))
-    return (q)
-}
-
-kappaStatistic = function(n11, n10, n01, n00, l){
-    if(n11 == l) {
-        return (0)
-    }
-    theta1 = (n11 + n00) / l
-    theta2 = (((n11 + n01) * (n11 + n10)) + ((n10 + n00) * (n01 + n00))) / (l ^2)
-    kappa = (theta1 - theta2) / (1 - theta2)
-    return (kappa)
-}
-
-diversityMean = function(ensemble, x){
-    if(length(ensemble) < 2){
-        print("Ensemble with 1 classifier")
-        return (list(0, 0))
-    }
-    q = c()
-    kappa = c()
-    n = nrow(x)
-    l = length(ensemble)
-    for(i in 1:(length(ensemble)-1)){
-        print(paste("Diversity pair:", i))
-        for(j in (i+1):length(ensemble)){            
-            test = testPair(ensemble[[i]], ensemble[[j]], x)
-            q = c(q, qStatistic(test[[1]], test[[2]], test[[3]], test[[4]], n))
-            kappa = c(kappa, kappaStatistic(test[[1]], test[[2]], test[[3]], test[[4]], n))
-        }
-    }
-
-    qSum = sum(q)
-    qSum = 2 * qSum / (l * (l-1))
-    kappaSum = sum(kappa)
-    kappaSum = 2 * kappaSum / (l * (l-1))
-
-    return (list(qSum, kappaSum))
-}
-
-reduceErrorPruning = function(pool, validationSet){
-   # Sort classifiers by accuracy
-    accuracy = c()
-    for(i in 1:length(pool)){
-        accuracy = c(accuracy, getEnsembleAccuracy(list(pool[[i]]), validationSet))
-    }
-    bestClassifierIdx = which.max(accuracy)
-    
-    ensemble = list()
-    ensemble[[length(ensemble) + 1]] = pool[[bestClassifierIdx]]
-    remainingIdx = 1:length(pool)
-    remainingIdx = remainingIdx[-bestClassifierIdx]
-
-    while(length(remainingIdx) > 0){
-        print(paste("Reduce Error: Emsemble Size: ", length(ensemble)))
-        currentAccuracy = getEnsembleAccuracy(ensemble, validationSet)
-
-        newAccuracy = c()
-        for(i in 1:length(remainingIdx)){
-            testEnsemble = ensemble
-            testEnsemble[[length(testEnsemble) + 1]] = pool[[remainingIdx[i]]]
-            newAccuracy = c(newAccuracy, getEnsembleAccuracy(testEnsemble, validationSet))
-        }
-        
-        bestClassifierIdx = which.max(newAccuracy)
-        newAccuracy = max(newAccuracy)
-        if((newAccuracy > currentAccuracy) || (newAccuracy >= currentAccuracy && length(ensemble) < 3)){
-            ensemble[[length(ensemble) + 1]] = pool[[remainingIdx[bestClassifierIdx]]]
-            remainingIdx = remainingIdx[-bestClassifierIdx]
-        }else{
-            break
-        }
-    }
-    return (ensemble)
-}
-
-reduceErrorPruningOrdered = function(pool, validationSet){
-   # Sort classifiers by accuracy
-    accuracy = c()
-    for(i in 1:length(pool)){
-        accuracy = c(accuracy, getEnsembleAccuracy(list(pool[[i]]), validationSet))
-    }
-    accuracyIdx = sort(accuracy, decreasing = TRUE,index.return=TRUE)$ix
-
-    ensemble = list()
-    ensemble[[length(ensemble) + 1]] = pool[[accuracyIdx[1]]]
-    accuracyIdx = accuracyIdx[-1]
-
-    while(length(accuracyIdx) > 0){
-        print(paste("Reduce Error: Emsemble Size: ", length(ensemble)))
-        currentAccuracy = getEnsembleAccuracy(ensemble, validationSet)
-
-        newAccuracy = c()
-        for(i in 1:length(accuracyIdx)){
-            testEnsemble = ensemble
-            testEnsemble[[length(testEnsemble) + 1]] = pool[[accuracyIdx[i]]]
-            newAccuracy = c(newAccuracy, getEnsembleAccuracy(testEnsemble, validationSet))
-        }
-        
-        bestClassifierIdx = which.max(newAccuracy)
-        newAccuracy = max(newAccuracy)
-        if(newAccuracy > currentAccuracy){
-            ensemble[[length(ensemble) + 1]] = pool[[accuracyIdx[bestClassifierIdx]]]
-            accuracyIdx = accuracyIdx[-bestClassifierIdx]
-        }else{
-            break
-        }
-    }
-    return (ensemble)
-}
-
-bestFirstPruning = function(pool, validationSet){
-    # Sort classifiers by accuracy
-    accuracy = c()
-    for(i in 1:length(pool)){
-        accuracy = c(accuracy, getEnsembleAccuracy(list(pool[[i]]), validationSet))
-    }
-    accuracyIdx = sort(accuracy, decreasing = TRUE,index.return=TRUE)$ix
-
-    # Mount the ensembles
-    ensembles = list()
-    accuracy = c()    
-    for(i in 1:length(pool)){
-        print(paste("Best First: Emsemble ", i))
-        if(i == 1){
-            ensembles[[i]] = list(pool[[accuracyIdx[i]]])
-        }else{
-            ensembles[[i]] = append(ensembles[[i-1]], list(pool[[accuracyIdx[i]]]))
-        }
-        accuracy = c(accuracy, getEnsembleAccuracy(ensembles[[i]], validationSet))
-    }
-    # plot(accuracy, ylim=c(0,1), type='l', main='Best First Pruning', xlab='Number of Classifiers on Ensamble', ylab='Accuracy')
-
-    bestEnsemble = ensembles[[max(which(accuracy == max(accuracy)))]] # Critério de desempate proposto
-    # bestEnsemble = ensembles[[which.max(accuracy)]]
-
-    return (bestEnsemble)
-}
-
-# http://www.cin.ufpe.br/~tg/2017-1/fnw_tg.pdf
-kdn = function(x, k){
-    target = x[,ncol(x)]
-    x = x[, -ncol(x)]
-
-    neighboorsIdx = kNN(x, k)$id
-    kdnValues = c()
-    for (i in 1:nrow(x)){
-        ## Calculate KDN 
-        disagreeingNeighboors = length(which(target[neighboorsIdx[i,]] != target[i]))
-        kdnValues = c(kdnValues, disagreeingNeighboors/k)
-    }
-    return (kdnValues)
-}
-
-bagging = function(x){
+bootstrap = function(x){
     numOfRows = nrow(x)
     x = x[sample(numOfRows, replace=T), ]
     rownames(x) = NULL
     return (x)
 }
 
-bootstrap = function(x){
-    return (bagging(x))
-}
-
-subset = function(x, percent){
-    numOfRows = floor(nrow(x) * percent)
-    x = x[sample(numOfRows),]
-    return (x)
-}
-
-subsetStratified = function(x, percent){
-    idx = which(x[,'target'] == 'true')
-    class1 = x[idx, ]
-    class2 = x[-idx, ]
-    x = rbind(subset(class1, percent), subset(class2, percent))
-    x = x[sample(nrow(x)),]
-}
-
-majorityVote = function(predicts){
-    return (names(which.max(table(predicts))))
-}
-
-getMetrics = function(predMatrix, target){
-     # Get majority vote
-    predFinal = list()
-    for(j in 1:length(target)){
-        predFinal[[j]] = majorityVote(predMatrix[,j])
-    }
-
-    # Calculate metrics
-    metrics = list()
-
-    predF = array(unlist(predFinal))
-    predF = factor(predF, levels = c('false','true'))
-
-    metrics$auc = roc.curve(target, predF, plotit = F)$auc
-    metrics$accuracy = confusionMatrix(predF, target)$overall['Accuracy']
-    metrics$fmeasure = confusionMatrix(predF, target)$byClass['F1']
-    metrics$gmean = sqrt(confusionMatrix(predF, target)$byClass['Precision'] * confusionMatrix(target, predF)$byClass['Recall'])
-
-    return (metrics)
-}
-
-getEnsembleAccuracy = function(ensemble, data){
-    # Predict on data dataset
-    target = data[, 'target']
-    data = data[, -ncol(data)]
-
-    pred = matrix(0, length(ensemble), nrow(data))
-    for(j in 1:length(ensemble)){
-        t = data[,ensemble[[j]][[1]]]
-        pred[j, ] = perceptron.test(t, ensemble[[j]][[2]])
-    }
-    
-    predFinal = list()
-    for(j in 1:length(target)){
-        predFinal[[j]] = majorityVote(pred[,j])
-    }
-
-    predF = array(unlist(predFinal))
-    predF = factor(predF, levels = c('false','true'))
-
-    accuracy = confusionMatrix(predF, target)$overall['Accuracy']
-    return (accuracy)
-}
-
-predictPerceptron = function(models, test){
+predictPool = function(pool, test, rule){
     # Predict on test dataset
     target = test[, 'target']
     test = test[, -ncol(test)]
 
-    pred = matrix(0, length(models), nrow(test))
-    for(j in 1:length(models)){
-        t = test[,models[[j]][[1]]]
-        pred[j, ] = perceptron.test(t, models[[j]][[2]])
+    predClassifier = data.frame(matrix(0, length(pool), nrow(test)))
+    for(j in 1:length(pool)){
+        predClassifier[j, ] = predict(pool[[j]], test)
     }
-    metrics = getMetrics(pred, target)
-    return (metrics)
+    
+    pred = list()
+    for(j in 1:length(target)){
+        pred[[j]] = rule(predClassifier[,j])
+    }
+    pred = array(unlist(pred))
+    pred = factor(pred, levels = c('false','true'))
+    return(pred)
 }
 
-perceptron.test = function(x, weights) {
-    target = c()
-    for (i in 1:nrow(x)){
-        label = 'true'
-        if (as.numeric(x[i,]) %*% weights[2:length(weights)] > 0){
-            label = 'false'
-        }
-        target = c(target, label)
-    }
-    return (target)
-}
-
-# https://rpubs.com/FaiHas/197581
-perceptron.train = function(x, eta, niter) {
-    target = x[,ncol(x)]
-    x = x[, -ncol(x)]    
-
-    y = rep (1, length(target))
-    y[target == "true"] = -1    
-    
-    # initialize weight vector
-    weight <- rep(0, dim(x)[2] + 1)
-    names = names(x)
-    errors <- rep(0, niter)    
-    
-    # loop over number of epochs niter
-    for (jj in 1:niter) {
-            
-        # loop through training data set
-        for (ii in 1:length(y)) {                
-            # Predict binary label using Heaviside activation 
-            # function
-            z <- sum(weight[2:length(weight)] * 
-                                as.numeric(x[ii, ])) + weight[1]
-            if(z < 0) {
-                    ypred <- -1
-            } else {
-                    ypred <- 1
-            }
-            
-            # Change weight - the formula doesn't do anything 
-            # if the predicted value is correct
-            weightdiff <- eta * (y[ii] - ypred) * 
-                    c(1, as.numeric(x[ii, ]))
-            weight <- weight + weightdiff
-            
-            # Update error function
-            if ((y[ii] - ypred) != 0.0) {
-                    errors[jj] <- errors[jj] + 1
-            }                
-        }
-    }    
-    # weight to decide between the two species 
-    return( list(names, weight))
+majorityVote = function(predicts){
+    return (names(which.max(table(predicts))))
 }
