bdc50bc83833216706b6c2199af0bcddfee5ccf7
Daniel Bion Barreiros <bion@manifestohut.com>
2018-11-17 15:45:05 -0300

    Cluster bal

--- projeto/main.r
+++ projeto/main.r
@@ -8,71 +8,20 @@ setwd(path)
 source('utils.r')
 
 NB = make_Weka_classifier("weka/classifiers/bayes/NaiveBayes")
-
-experiments = function(data, classifier, rules){
-    numOfFolds = 10
-    
-    # Padronizar variáveis explicativas entre 0 e 1
-    x = as.data.frame(apply(data[, -ncol(data)], 2, standardize))
-    x$target = data[, ncol(data)]
-    
-    # Quebrar dataset em 10 folds divididos em treino e teste
-    x = splitInFolds(x, numOfFolds)
-    auc = list()
-
-    for (i in 1:numOfFolds) {
-        trainSet = x[[i]]$train
-        testSet = x[[i]]$test
-
-        # Separar classe minoritária da majoritária
-        majorityClass = trainSet[which(trainSet$target == 'negative'), ]
-        minorityClass = trainSet[which(trainSet$target == 'positive'), ]
-
-        # Calcular número de bins
-        numOfBins = floor(nrow(majorityClass) / nrow(minorityClass))
-
-        # Criar bins juntando a classe minoritária
-        binsIdx = createFolds(majorityClass$target, k = numOfBins)
-        bins = list()
-        for(j in 1:length(binsIdx)){
-            bins[[j]] = rbind(majorityClass[binsIdx[[j]], ], minorityClass)
-            bins[[j]] = bootstrap(bins[[j]])
-        }
-
-        # Treinar o classificador base escolhido para cada bin
-        pool = list()
-        for(j in 1:length(bins)){
-            pool[[j]] = classifier(target ~ ., bins[[j]])
-        }
-
-        # Aplicar modelo no conjunto de teste para cada regra de combinação
-        auc[[i]] = list()
-        for(j in 1:length(rules)){
-            p = predictPool(pool, testSet, rules[[j]])
-            auc[[i]][[j]] = roc.curve(testSet$target, p, plotit = F)$auc
-        }
-    }
-
-    return (auc)
-}
-
-data = read.table('data3.csv' , sep=',', header=T)
+rules = list(majorityVote, maxRule, minRule, prodRule, sumRule)
 
 files = list.files('data/')
 result = list()
-
-rules = list(majorityVote, maxRule, minRule, prodRule, sumRule)
-
+# SplitBal
 for(i in 1:length(files)){
     print(i)
     data = read.table(paste('data/', files[i], sep = ''), sep=',', header=F)
 
     result[[i]] = list()
-    result[[i]]$naiveBayes = experiments(data, NB, rules)
-    result[[i]]$J48 = experiments(data, J48, rules)
-    result[[i]]$JRip = experiments(data, JRip, rules)
-    result[[i]]$SMO = experiments(data, SMO, rules)
-    result[[i]]$IBK = experiments(data, IBk, rules)
-    result[[i]]$randomForest = experiments(data, randomForest, rules)
+    result[[i]]$naiveBayes = experiments(data, NB, rules, splitBal)
+    result[[i]]$J48 = experiments(data, J48, rules, splitBal)
+    result[[i]]$JRip = experiments(data, JRip, rules, splitBal)
+    result[[i]]$SMO = experiments(data, SMO, rules, splitBal)
+    result[[i]]$IBK = experiments(data, IBk, rules, splitBal)
+    result[[i]]$randomForest = experiments(data, randomForest, rules, splitBal)
 }
-
--- projeto/utils.r
+++ projeto/utils.r
@@ -28,7 +28,7 @@ bootstrap = function(x){
 }
 
 predictPool = function(pool, test, rule){
-    # Predict on test dataset
+    # Predict no test dataset
     target = test[, 'target']
     test = test[, -ncol(test)]
 
@@ -36,14 +36,24 @@ predictPool = function(pool, test, rule){
     prob1 = hard
     prob2 = hard
     for(j in 1:length(pool)){
-        p = predict(pool[[j]], test, type = 'probability')
+        # Predict probability
+        p = tryCatch({
+                predict(pool[[j]], test, type = 'probability')
+            }, 
+            error = function(e) {
+                predict(pool[[j]], test, type = 'prob')
+            }
+        )
+
         prob1[j, ] = p[, 1]
         prob2[j, ] = p[, 2]
+        # Predict hard
         hard[j, ] = predict(pool[[j]], test)
     }
     
     pred = list()
     for(j in 1:length(target)){
+        # Aplicar a regra escolhida
         pred[[j]] = rule(hard[, j], prob1[, j], prob2[, j])
     }
     pred = array(unlist(pred))
@@ -69,4 +79,65 @@ prodRule = function(hard, prob1, prob2){
 
 sumRule = function(hard, prob1, prob2){
     return (ifelse(sum(prob1) > sum(prob2), 'negative', 'positive'))
+}
+
+splitBal = function(majorityClass, numOfBins){
+    binsIdx = createFolds(majorityClass$target, k = numOfBins)
+    return (binsIdx)
+}
+
+clusterBal = function(majorityClass, numOfBins){
+    kIdx = kmeans(majorityClass[, -ncol(majorityClass)], numOfBins)$cluster
+    binsIdx = list()
+    for(j in 1:numOfBins){
+        binsIdx[[j]] = which(kIdx == j)
+    }
+    return (binsIdx)
+}
+
+experiments = function(data, classifier, rules, splitMethod){
+    numOfFolds = 10
+    
+    # Padronizar variáveis explicativas entre 0 e 1
+    x = as.data.frame(apply(data[, -ncol(data)], 2, standardize))
+    x$target = data[, ncol(data)]
+    
+    # Quebrar dataset em 10 folds divididos em treino e teste
+    x = splitInFolds(x, numOfFolds)
+    auc = list()
+
+    for (i in 1:numOfFolds) {
+        trainSet = x[[i]]$train
+        testSet = x[[i]]$test
+
+        # Separar classe minoritária da majoritária
+        majorityClass = trainSet[which(trainSet$target == 'negative'), ]
+        minorityClass = trainSet[which(trainSet$target == 'positive'), ]
+
+        # Calcular número de bins
+        numOfBins = floor(nrow(majorityClass) / nrow(minorityClass))
+
+        # Criar bins juntando a classe minoritária
+        binsIdx = splitMethod(majorityClass, numOfBins)
+    
+        bins = list()
+        for(j in 1:length(binsIdx)){
+            bins[[j]] = rbind(majorityClass[binsIdx[[j]], ], minorityClass)
+            bins[[j]] = bootstrap(bins[[j]])
+        }
+
+        # Treinar o classificador base escolhido para cada bin
+        pool = list()
+        for(j in 1:length(bins)){
+            pool[[j]] = classifier(target ~ ., bins[[j]])
+        }
+
+        # Aplicar modelo no conjunto de teste para cada regra de combinação
+        auc[[i]] = list()
+        for(j in 1:length(rules)){
+            p = predictPool(pool, testSet, rules[[j]])
+            auc[[i]][[j]] = roc.curve(testSet$target, p, plotit = F)$auc
+        }
+    }
+    return (auc)
 }
